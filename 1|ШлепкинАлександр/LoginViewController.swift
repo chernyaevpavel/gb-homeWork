d61596b1c9d36ef44ae91215b1438af2a9ea1122
Aleksandr <Shlepkinaleksandr@mail.ru>
2021-04-28 13:23:33 +0300

    Lesson 1-2

---
+++ 1|ШлепкинАлександр/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.swift
+//  1|ШлепкинАлександр
+//
+//  Created by Александр Шлепкин on 24.04.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var loginPutText: UITextField!
+    
+    @IBOutlet weak var passowrdPutText: UITextField!
+    
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard identifier == "loginSegue" else {
+            return false
+        }
+        let isLoginPasswordCorrect = loginPutText.text == "1" && passowrdPutText.text == "1"
+        if isLoginPasswordCorrect {
+            return true
+        } else {
+            showErrorAlert()
+        }
+        return false
+    }
+    
+    
+    private func showErrorAlert() {
+        let alert = UIAlertController(title: "Ошибка", message: "Не верный пароль", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel) { [weak self] _ in
+            self?.loginPutText.text = ""
+            self?.passowrdPutText.text = ""
+        }
+        
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+      
+    }
+
+}
---
+++ 1|ШлепкинАлександр/secondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  secondViewController.swift
+//  1|ШлепкинАлександр
+//
+//  Created by Александр Шлепкин on 24.04.2021.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
